eb43ce9046d1c303a8ccedb17f06ee32f8c24093
Shaurya Mishra <111196842+Shaurya-Mishra@users.noreply.github.com>
2022-09-09 15:52:52 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,19 @@
+package com.pack1;
+
+public class forloop {
+	void meth1()
+	{
+		for(int i= 1;i<=10;i++)
+		{
+			System.out.println("i value:" +i);
+		}
+	}
+
+	public static void main(String[] args) 
+	{
+		new forloop().meth1();
+		
+
+	}
+
+}
